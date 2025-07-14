c3e50f53bb8ad295815519b7d75dbe6c67a12ef0
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-07-13 23:38:40 -0400

    FPT2 MeZO Edge Pruning (#12)
    * Added EdgePruning codebase
    
    * Fix env file
    
    * Update README.md
    
    * Update installation instructions
    
    * Updated repo structure
    
    * renamed conda env
    
    * Update README.md
    
    * created eval notebooks
    
    * Update environment.yml
    
    * added gt_sweep.sh
    
    * add questions
    
    * update format of gt.py
    
    * Updated run script
    
    * fixed stuff
    
    * Fix stuff
    
    * update imports
    
    * upload data zip
    
    * update _inner_training_loop for transformers 4.45.2
    
    * update dev instructions
    
    * Delete data directory
    
    * Update .gitignore
    
    * Update .gitignore
    
    * fixed logging
    
    * added args
    
    * move mezo to new file
    
    * Update README.md
    
    * Update mezo.py
    
    * fix logger
    
    * migrated custom
    
    * migrated mezo fpt2-ioi
    
    * migrated mezo fpt2-gp
    
    * added original script
    
    * updated gp and ioi script
    
    * fix path
    
    * removed jupyter
    
    * update instructions
    
    * Create EDGE_ioi_sweep.sh
    
    * fix import path
    
    * fix edgeioisweep
    
    * Create gpu_logger.sh
    
    * fix gpu logg
    
    * fix
    
    * Update gpu_logger.py
    
    * Create EDGE_gt_sweep.sh
    
    * update readme with google oauth instructions
    
    * Revert "update readme with google oauth instructions"
    
    This reverts commit a4a94ee5d2ef254e64052bcf35b0adc8da5d1890.
    
    * Update README.md
    
    * rename MAAA --> edge_pruning_mezp
    
    Renamed src/layer2/prune/MAAA to src/layer2/prune/edge_pruning_mezo.

---
+++ setup.sh
@@ -0,0 +1,29 @@
+# in development - not sure if this works
+BRANCH_NAME="test"
+ENV_NAME="MAAA_CD"
+CLONE=true
+
+apt-get update
+apt-get upgrade
+
+apt-get install unzip
+
+# Install Mini conda
+# make this optional / argumented
+# make this an arg
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
+source ~/miniconda3/bin/activate
+conda init --all
+source ~/.bashrc
+
+git clone https://github.com/alansun17904/MAAA.git
+cd MAAA
+git checkout $BRANCH_NAME
+
+conda env create -f environment.yml
+conda activate $ENV_NAME
+
+unzip data.zip

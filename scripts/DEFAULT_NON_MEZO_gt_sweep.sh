82b831e9905dccc91243e5fcf3f8f7e2b106896e
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-07-21 19:09:08 -0400

    add wandb logging

--- scripts/DEFAULT_NON_MEZO_gt_sweep.sh
+++ scripts/DEFAULT_NON_MEZO_gt_sweep.sh
@@ -30,7 +30,7 @@ N_VAL=150 # The val split size
 # If you want to always keep embedding nodes, remove the --with_embedding_nodes flag
 # That flag, when set, also models masks over the embedding nodes
 
-WANDB_PROJECT=MAAA_CD_MEZO WANDB_MODE=online python src/layer2/prune/${VERSION}/fpt2_gt.py \
+WANDB_WATCH=all WANDB_PROJECT=MAAA_CD_MEZO WANDB_MODE=online python src/layer2/prune/${VERSION}/fpt2_gt.py \
     --report_to wandb \
     --run_name "DEFAULT_NON_MEZO_gt-${VERSION}-${TAG}-elr${ELR}-llr${LLR}-relr${RELR}-rllr${RLLR}-es${EDGE_SPARSITY}-ns${NODE_SPARSITY}-t${TOTAL}-$(date +%Y%m%d_%H%M%S)" \
     --do_train \

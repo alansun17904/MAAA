3279b52c61b091df6fa0d28136d9884c19624bd4
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-07-24 11:44:24 -0400

    Use optimizer LR

--- src/layer2/prune/edge_pruning_mezo/mezo.py
+++ src/layer2/prune/edge_pruning_mezo/mezo.py
@@ -859,7 +859,8 @@ class MeZOTrainer(Seq2SeqTrainer):
         return loss1
     
     def zo_get_lr(self, group):
-        return self.lr_scheduler.get_last_lr()[group-1]
+        return self.optimizer.param_groups[group]["lr"]
+        # return self.lr_scheduler.get_last_lr()[group-1]
 
 
     def zo_update(self, model):
@@ -888,6 +889,7 @@ class MeZOTrainer(Seq2SeqTrainer):
                 param.data = param.data - self.zo_get_lr(1) * (self.projected_grad * z) # where does self._get_learning_rate() interface w/ the multiple LRs
                 # print(f'Other LR: {self.zo_get_lr(1)}')
 
+        self.optimizer.step()
         self.lr_scheduler.step()
     
 @dataclass

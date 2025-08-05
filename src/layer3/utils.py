55c6fe4889f8099b89b67b49bbd956ee88331792
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-08-05 12:36:03 -0400

    Update utils.py

--- src/layer3/utils.py
+++ src/layer3/utils.py
@@ -4,19 +4,20 @@ from datasets import load_dataset
 from functools import partial
 
 
-def ca_load_dataset(path, model : AutoModelForCausalLM, tokenizer : AutoTokenizer, device):
+def ca_load_dataset(path, model : AutoModelForCausalLM, tokenizer : AutoTokenizer, device, max_new_tokens):
     # In the future, we can refactor this to account for the various different types of datasets or smth
     dataset = load_dataset('csv', data_files=path)
     dataset.set_format(type='torch')
 
-    dataset = dataset.map(partial(generate_data, model, tokenizer, device), batched=True)
-    dataset = dataset.map()
+    dataset = dataset.map(partial(generate_data, model, tokenizer, device, max_new_tokens), batched=True)
+    dataset = dataset.map() # yo wtf
     return dataset
 
 
-def generate_data(model : AutoModelForCausalLM, tokenizer : AutoTokenizer, device, example):
-    tokens = tokenizer(example['corrupted'], return_tensors='pt').to(device)
+def generate_data(model : AutoModelForCausalLM, tokenizer : AutoTokenizer, device, tokens, example):
+    tokens = tokenizer(example['corrupted'], return_tensors='pt', padding=True, padding_side='left').to(device)
     with torch.no_grad():
-        example['corr_logits'] = model.generate(**tokens, max_new_tokens=10).logits
+        out = model.generate(**tokens, max_new_tokens=tokens, return_dict_in_generate=True, output_logits=True)
+        example['corr_logits'] = torch.transpose(torch.stack(out.logits), 0, 1)
     # Fix device & max new tokens
     return example

febc673716ea61ec4179d7f33922899a6d2e901c
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-08-06 13:34:02 -0400

    Update modeling_fpt2.py

--- src/layer2/modeling/modeling_fpt2.py
+++ src/layer2/modeling/modeling_fpt2.py
@@ -884,8 +884,8 @@ class FPT2Model(FPT2PreTrainedModel):
             # self.sparsity_lambda_nodes_1 = nn.Parameter(torch.tensor([0.0], dtype=self._dtype))
             self.sparsity_lambda_edges_1 = torch.tensor([0], dtype=self._dtype)
             self.sparsity_lambda_nodes_1 = torch.tensor([0], dtype=self._dtype)
-        self.sparsity_lambda_edges_2 = nn.Parameter(torch.tensor([0], dtype=self._dtype))
-        self.sparsity_lambda_nodes_2 = nn.Parameter(torch.tensor([0], dtype=self._dtype))
+        self.sparsity_lambda_edges_2 = nn.Parameter(torch.tensor([2], dtype=self._dtype))
+        self.sparsity_lambda_nodes_2 = nn.Parameter(torch.tensor([2], dtype=self._dtype))
         # self.sparsity_lambda_edges_2 = torch.tensor([50], dtype=self._dtype)
         # self.sparsity_lambda_nodes_2 = torch.tensor([50], dtype=self._dtype)
 

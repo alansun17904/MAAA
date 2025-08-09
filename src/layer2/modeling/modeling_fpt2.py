97b41004cfbfe15e9360eab9ec303472b481dacb
Mustafa Khan <74122733+MustafaKhan0@users.noreply.github.com>
2025-08-08 22:22:52 -0400

    switch to linear not exponential loss

--- src/layer2/modeling/modeling_fpt2.py
+++ src/layer2/modeling/modeling_fpt2.py
@@ -882,12 +882,12 @@ class FPT2Model(FPT2PreTrainedModel):
         else:
             # self.sparsity_lambda_edges_1 = nn.Parameter(torch.tensor([0.0], dtype=self._dtype))
             # self.sparsity_lambda_nodes_1 = nn.Parameter(torch.tensor([0.0], dtype=self._dtype))
-            self.sparsity_lambda_edges_1 = torch.tensor([0], dtype=self._dtype)
-            self.sparsity_lambda_nodes_1 = torch.tensor([0], dtype=self._dtype)
-        self.sparsity_lambda_edges_2 = nn.Parameter(torch.tensor([4], dtype=self._dtype))
-        self.sparsity_lambda_nodes_2 = nn.Parameter(torch.tensor([4], dtype=self._dtype))
-        # self.sparsity_lambda_edges_2 = torch.tensor([4], dtype=self._dtype)
-        # self.sparsity_lambda_nodes_2 = torch.tensor([4], dtype=self._dtype)
+            self.sparsity_lambda_edges_1 = torch.tensor([-4], dtype=self._dtype)
+            self.sparsity_lambda_nodes_1 = torch.tensor([-4], dtype=self._dtype)
+        # self.sparsity_lambda_edges_2 = nn.Parameter(torch.tensor([4], dtype=self._dtype))
+        # self.sparsity_lambda_nodes_2 = nn.Parameter(torch.tensor([4], dtype=self._dtype))
+        self.sparsity_lambda_edges_2 = torch.tensor([0], dtype=self._dtype)
+        self.sparsity_lambda_nodes_2 = torch.tensor([0], dtype=self._dtype)
 
         # Initialize weights and apply final processing
         self.post_init()

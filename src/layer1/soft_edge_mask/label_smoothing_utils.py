a87fa34e99bb15167127e8719b630c064c62c94c
Aaron Wei <hellobob360360360@gmail.com>
2025-08-20 21:50:43 -0400

    Fixed some errors

--- src/layer1/soft_edge_mask/label_smoothing_utils.py
+++ src/layer1/soft_edge_mask/label_smoothing_utils.py
@@ -1,17 +1,16 @@
 import math
 import json
 
-def compute_z_star(mask: dict, alpha: float = 0.1) -> dict:
+def compute_z_star(pruneinfo: list, alpha: float = 0.1) -> dict:
     """
     Compute z_i* = 1 - alpha if retained (mask==1), else alpha if pruned (mask==0).
     Input: {"block.0.attn.W_0": {"score": ..., "mask": 1 or 0}, ...}
     Output: {"block.0.attn.W_0": 0.9, ...}
     """
     return {
-        k: (1 - alpha if v["mask"] == 1 else alpha)
-        for k, v in mask.items()
+        f"{info['name']}[{info['head_idx']}]" if "mlp" not in info['name'] else info['name']: (1 - alpha if info["mask"] == 1 else alpha)
+        for info in pruneinfo
     }
-
 def compute_edge_scores(z_star: dict) -> dict:
     """
     Compute P(z_i → z_j) = sqrt(z_i* * z_j*) for all component pairs.
@@ -19,15 +18,23 @@ def compute_edge_scores(z_star: dict) -> dict:
     """
     components = list(z_star.keys())
     edge_scores = {}
-
+    writers = {}
     for comp_i in components:
+        if "W_O" in comp_i or "W_out" in comp_i:
+            writers[comp_i] = z_star[comp_i]
+            continue
+        layeri = int(comp_i.split(".")[1])
+        name = f"{comp_i.split('.')[2]}.{comp_i.split('.')[-1]}"
         edge_scores[comp_i] = {}
-        for comp_j in components:
-            if comp_i == comp_j:
-                continue  # skip self-loops
+        for comp_j in components: #Really inefficient frn but whatever
+            layerj = int(comp_j.split(".")[1])
+            name = f"{comp_j.split('.')[2]}.{comp_i.split('.')[-1]}"
+            if (comp_i == comp_j) or (layerj >= layeri) or ("W_out" not in comp_j and ("W_O" not in comp_j)):
+                continue  # skips if not writer
+            
             score = math.sqrt(z_star[comp_i] * z_star[comp_j])
             edge_scores[comp_i][comp_j] = round(score, 4)
-    return edge_scores
+    return edge_scores, writers
 
 def load_mask(filepath: str) -> dict:
     """
